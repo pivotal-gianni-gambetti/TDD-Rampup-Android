0297e05796d87fc7453f42afb0055fb2b6c45586
GavinG/GianniG <ggambetti@pivotallabs.com>
2014-02-21 09:27:38 -0500

    Fixing build

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 echo "Installing android support library..."
 echo "                                     from ${ANDROID_HOME}."
 mvn -q install:install-file -DgroupId=com.android.support -DartifactId=support-v4 \
-	-Dversion=19.0.0 -Dpackaging=jar -Dfile="$ANDROID_HOME/sdk/extras/android/support/v4/android-support-v4.jar"
+	-Dversion=19.0.0 -Dpackaging=jar -Dfile="$ANDROID_HOME/extras/android/support/v4/android-support-v4.jar"
 
